8384ff4d25e9c7cef9c4f4caf3f0dbc069cc2b56
fgazzelloni <docksbox@pm.me>
2022-06-05 10:19:11 +0200

    updating

--- R/notes.R
+++ R/notes.R
@@ -1,9 +1,11 @@
 # to see your site in the viewer pane
 blogdown::serve_site()
 
-
+blogdown::build_site()
 # all the blog content is in the content folder/blog
 # after have made some changes
 # pull, commit and push to GitHub
 
 # cmd + a (select all commits)
+
+blogdown::stop_server()
