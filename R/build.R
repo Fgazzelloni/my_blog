9df7856ab7d21b5f452f225bceb36ebe5f7133be
Federica Gazzelloni <docksbox@pm.me>
2022-07-22 01:10:42 +0200

    eu flights

--- R/build.R
+++ R/build.R
@@ -1,2 +1,10 @@
 # An optional custom script to run before Hugo builds your site.
 # You can delete it if you do not need it.
+
+
+# How to fix undesired commits:
+
+# https://stackoverflow.com/questions/16288176/your-branch-is-ahead-of-origin-master-by-3-commits
+
+# on command line: git reset --hard origin
+# it will erase all the unwanted commits
